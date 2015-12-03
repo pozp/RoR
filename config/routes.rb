f6d710d990eb9d3b3016509d528f6ff475e6523b
pozp <pawel.pozdziej@o2.pl>
2015-12-03 09:47:12 +0000

    upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_page#home'
-  get  'static_page/portfolio'
-  get  'static_page/about'
-  get  'static_page/contact'
+  get 'portfolio' => 'static_page#portfolio'
+  get  'about' =>'static_page#about'
+  get  'contact' => 'static_page#contact'
+  get 'signup' =>'users#new'
 end
--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPageControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Paweł Poździej"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Paweł Poździej"
   end
   
   test "should get portfolio" do
     get :portfolio
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Paweł Poździej"
   end
   
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Paweł Poździej"
   end
 
 end
